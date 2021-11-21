77a0ba9f00f96fd3ec3ca99c011387c139dd2aad
PLP-GTR <github@plpgtr.de>
2021-11-22 00:47:46 +0100

    Formatting

--- src/app/Models/FederalState.php
+++ src/app/Models/FederalState.php
@@ -10,22 +10,22 @@ class FederalState extends Model
 {
     use HasFactory;
 
-    const STATE_BADEN_WUERTTEMBERG = 'BADEN_WUERTTEMBERG';
-    const STATE_BAVARIA = 'BAYERN';
-    const STATE_BERLIN = 'BERLIN';
-    const STATE_BRANDENBURG = 'BRANDENBURG';
-    const STATE_BREMEN = 'BREMEN';
-    const STATE_HAMBURG = 'HAMBURG';
-    const STATE_HESSE = 'HESSEN';
-    const STATE_LOWER_SAXONY = 'NIEDERSACHSEN';
+    const STATE_BADEN_WUERTTEMBERG            = 'BADEN_WUERTTEMBERG';
+    const STATE_BAVARIA                       = 'BAYERN';
+    const STATE_BERLIN                        = 'BERLIN';
+    const STATE_BRANDENBURG                   = 'BRANDENBURG';
+    const STATE_BREMEN                        = 'BREMEN';
+    const STATE_HAMBURG                       = 'HAMBURG';
+    const STATE_HESSE                         = 'HESSEN';
+    const STATE_LOWER_SAXONY                  = 'NIEDERSACHSEN';
     const STATE_MECKLENBURG_WESTERN_POMERANIA = 'MECKLENBURG_VORPOMMERN';
-    const STATE_NORTH_RHINE_WESTPHALIA = 'NORDRHEIN_WESTFALEN';
-    const STATE_RHINELAND_PALATINATE = 'RHEINLAND_PFALZ';
-    const STATE_SAARLAND = 'SAARLAND';
-    const STATE_SAXONY = 'SACHSEN';
-    const STATE_SAXONY_ANHALT = 'SACHSEN_ANHALT';
-    const STATE_SCHLESWIG_HOLSTEIN = 'SCHLESWIG_HOLSTEIN';
-    const STATE_THURINGIA = 'THUERINGEN';
+    const STATE_NORTH_RHINE_WESTPHALIA        = 'NORDRHEIN_WESTFALEN';
+    const STATE_RHINELAND_PALATINATE          = 'RHEINLAND_PFALZ';
+    const STATE_SAARLAND                      = 'SAARLAND';
+    const STATE_SAXONY                        = 'SACHSEN';
+    const STATE_SAXONY_ANHALT                 = 'SACHSEN_ANHALT';
+    const STATE_SCHLESWIG_HOLSTEIN            = 'SCHLESWIG_HOLSTEIN';
+    const STATE_THURINGIA                     = 'THUERINGEN';
 
     /**
      * Returns the hospital info of the requested state
